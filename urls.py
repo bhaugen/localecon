1d0525839e5ba870194d54a71852e00eac3582c0
Bob Haugen <bob.haugen@gmail.com>
2009-09-11 15:36:44 -0500

    added login urlconfig

--- urls.py
+++ urls.py
@@ -14,6 +14,7 @@ urlpatterns = patterns('',
     # admin
     (r'^admin/doc/', include('django.contrib.admindocs.urls')),
     (r'^admin/', include(admin.site.urls)),
+    (r'^accounts/login/$', 'django.contrib.auth.views.login'),
 )
 
 if settings.DEBUG:
