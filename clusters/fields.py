15bdb70d336934f579cf3f6a8c6b4c0d9a42d84b
Bob Haugen <bob.haugen@gmail.com>
2009-11-18 17:05:38 -0600

    fixing ResourceTypeField 2

--- clusters/fields.py
+++ clusters/fields.py
@@ -7,7 +7,7 @@ class ResourceChoiceField(forms.ModelChoiceField):
 
     def __init__(self, *args, **kwargs):
         super(ResourceChoiceField, self).__init__(*args, **kwargs)
-        self.widget = forms.widgets.RelatedFieldWidgetWrapper(
+        self.widget = admin.widgets.RelatedFieldWidgetWrapper(
             forms.widgets.Select(),
             FunctionResourceType._meta.get_field('resource_type').rel,
             admin.site,
