b9c9bb7440db98d6d482fe099206ca22ecf16af3
Bob Haugen <bob.haugen@gmail.com>
2009-11-23 14:39:27 -0600

    refactoring site structure 1

--- clusters/admin.py
+++ clusters/admin.py
@@ -35,7 +35,7 @@ class EconomicAgentAdmin(admin.ModelAdmin):
 admin.site.register(EconomicAgent, EconomicAgentAdmin)
 
 admin.site.register(EconomicResourceType)
-dmin.site.register(SiteSettings)
+admin.site.register(SiteSettings)
 admin.site.register(CommunityResourceType)
 admin.site.register(CommunityAgent)
 
