9164a39ca2ac3009b42604b9ce3e89b29da97459
Bob Haugen <bob.haugen@gmail.com>
2009-12-11 08:42:14 -0600

    adding new agent cluster resources 33

--- clusters/forms.py
+++ clusters/forms.py
@@ -108,7 +108,7 @@ class AgentFunctionForm(forms.ModelForm):
           
 
 class AgentResourceForm(forms.ModelForm):
-    name = forms.CharField(required=False, widget=forms.TextInput(attrs={'size': '16', 'class': 'resource-name'}))
+    name = forms.CharField(required=False, widget=forms.TextInput(attrs={'size': '16', 'class': 'resource-name ac_input'}))
     amount = forms.IntegerField(required=False, widget=forms.TextInput(attrs={'size': '6', 'value': '0'}))
     
     def __init__(self, function_resource, *args, **kwargs):
