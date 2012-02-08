6fd8a0e0fff10ed0690b0af8443cde62f1ab9a83
Bob Haugen <bob.haugen@gmail.com>
2012-02-08 16:55:26 -0600

    experiment with Raphael edges

--- site_media/scripts/network.js
+++ site_media/scripts/network.js
@@ -1,15 +1,12 @@
-var redraw, g, renderer;
+var redraw;
 
-/* only do all this when document has finished loading (needed for RaphaelJS) */
 window.onload = function() {
     
     var width = $(document).width();
     var height = $(document).height() - 100;
 	
 	   var render = function(r, n) {
-            /* the Raphael set is obligatory, containing all you want to display */
             var set = r.set().push(
-                /* custom objects go here */
                 r.rect(n.point[0]-30, n.point[1]-13, 60, 44).attr({"fill": "#feb", r : "12px", "stroke-width" : n.distance == 0 ? "3px" : "1px" })).push(
                 r.text(n.point[0], n.point[1] + 10, (n.label || n.id)));
             return set;
@@ -28,10 +25,8 @@ window.onload = function() {
 		{% endfor %}
 	{% endfor %}
  
-    /* layout the graph using the Spring layout implementation */
     var layouter = new Graph.Layout.Spring(g);
     
-    /* draw the graph using the RaphaelJS draw implementation */
     renderer = new Graph.Renderer.Raphael('canvas', g, width, height);
     
     redraw = function() {
