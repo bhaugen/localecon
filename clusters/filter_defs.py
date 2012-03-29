4dde14bdd44289465d57e87fcc845dd9eea395a0
Bob Haugen <bob.haugen@gmail.com>
2012-03-29 16:38:08 -0500

    add experimental agent report using django_filters

--- clusters/filter_defs.py
+++ clusters/filter_defs.py
@@ -5,7 +5,7 @@ from models import *
 class AgentFilterSet(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_type='contains')
     address = django_filters.CharFilter(lookup_type='contains')
-    communities__name = django_filters.CharFilter(label='Community Name', lookup_type='contains')
+    communities__community__name = django_filters.CharFilter(label='Community Name', lookup_type='contains')
 
     class Meta:
         model = EconomicAgent
