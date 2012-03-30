ae0d0b0ec0db47d03b97208aa25acd91b6263fd6
Bob Haugen <bob.haugen@gmail.com>
2012-03-30 08:15:05 -0500

    tweak AgentFilterSet

--- clusters/filter_defs.py
+++ clusters/filter_defs.py
@@ -5,11 +5,12 @@ from models import *
 class AgentFilterSet(django_filters.FilterSet):
     name = django_filters.CharFilter(lookup_type='contains')
     address = django_filters.CharFilter(lookup_type='contains')
-    communities__community__name = django_filters.CharFilter(label='Community Name', lookup_type='contains')
+    #communities__community__name = django_filters.CharFilter(label='Community Name', lookup_type='contains')
 
     class Meta:
         model = EconomicAgent
-        exclude = ('latitude', 'longitude', 'slug')
+        #exclude = ('latitude', 'longitude', 'slug')
+        fields = ('communities__community',)
 
     @classmethod
     def queryset(cls):
