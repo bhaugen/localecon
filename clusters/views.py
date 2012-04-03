d98ed28c87e2223db5c74c08bf9cf1ac17c36af2
Bob Haugen <bob.haugen@gmail.com>
2012-04-03 10:21:08 -0500

    add io_vs_flows to diagnostics

--- clusters/views.py
+++ clusters/views.py
@@ -769,7 +769,7 @@ def diagnostics(request, cluster_id, level="fn"):
         function_production_without_consumption = cluster.agent_function_production_without_consumption()
         function_consumption_without_production = cluster.agent_function_consumption_without_production()
         if cluster.has_flows():
-            function_io_vs_flows = cluster.agent_io_vs_flows()
+            #function_io_vs_flows = cluster.agent_io_vs_flows()
     else:
         function_production_without_consumption = cluster.function_production_without_consumption()
         function_consumption_without_production = cluster.function_consumption_without_production()
