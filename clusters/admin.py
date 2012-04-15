785f4d9dd77559517713798f1becd106abdad28e
Bob Haugen <bob.haugen@gmail.com>
2012-04-15 10:48:18 -0500

    fix admin search field bug

--- clusters/admin.py
+++ clusters/admin.py
@@ -32,7 +32,7 @@ class OutgoingFunctionResourceFlowInline(admin.TabularInline):
 class EconomicFunctionAdmin(admin.ModelAdmin):
     list_display = ('name', 'cluster')
     list_filter = ['cluster',]
-    search_fields = ['name', 'cluster.name']
+    search_fields = ['name',]
     inlines = [ 
                FunctionResourceInline,
                IncomingFunctionResourceFlowInline,
